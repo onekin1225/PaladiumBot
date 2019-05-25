489e08e96a40cf0212d9ed158a31f9155c8ed935
Androz2091 <androz2091@gmail.com>
2019-05-25 11:39:57 +0200

    Add a sample config file

---
+++ data/config.sample.js
@@ -0,0 +1,16 @@
+module.exports = {
+    token:"Your Bot secret token",
+    logs:"The logs channel ID (for new servers)",
+    prefix:"p!",
+    color:"#FF0000",
+    owner:"The owner ID",
+    palaIP:"proxy.paladium-pvp.fr",
+    emotes:{ // An array of your custom emojis : <:EmojiName:EmojiID> (animated emoji <a:EmojiName:EmojiID>)
+        error:"<:error:XXXXXXXXXXXX>",
+        success:"<:success:XXXXXXXXXXXX>",
+        loading:"<a:loading:XXXXXXXXXXXX>",
+        on:"<:statusOn:XXXXXXXXXXXX>",
+        off:"<:statusOff:XXXXXXXXXXXX>",
+        player:"<:player:XXXXXXXXXXXX>"
+    }
+}
